e3e332878f8be14f31e8bca42214500a9a7238ea
bobAnthonyVarley <116462213+bobanothonyvarley@users.noreply.github.com>
2022-12-20 23:26:11 +0000

    Update How_Gonzaga_determines_academic_fees.py

--- How_Gonzaga_determines_academic_fees.py
+++ How_Gonzaga_determines_academic_fees.py
@@ -1,5 +1,5 @@
-#in this program, I will determine a student's characteristics, as well as if he recieves a discount on fees
-# this is my first time creating a class in python too
+#in this program, I will determine if a student recieves a discount on fees
+# this is my first time creating a class in python btw
 
 class Determining_Academic_Fees:
     def __init__(self, firstname, lastname, year, plays_rugby):
@@ -9,7 +9,7 @@ class Determining_Academic_Fees:
         self.playsrugby = plays_rugby
         
     def determine_fees(self):
-        fees = 7400 #actual fees for Gonzaga for 2022/23
+        fees = 7400 #actual annual (non-boarding) fees for Gonzaga for 2022/23, in €
         if (self.year<1 or self.year>6):
             fees = 0
         elif (self.playsrugby=='True'):
@@ -35,6 +35,6 @@ plays_Rugby = (input('Do you play rugby (True or False):\n>'))
 
 user = Determining_Academic_Fees(first_name, last_name, academic_year, plays_Rugby)
 
-user.determine_fees()
+user.determine_fees() #or Determining_Academic_Fees.determine_fees(user)
 
 # python determine_academic_fees.py
